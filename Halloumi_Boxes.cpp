58f03aa533bb98daf36bc05136c135317e59eb9b
Rishik Chakravarty <90590550+rishikrc3@users.noreply.github.com>
2024-03-21 22:45:28 +0530

    Update_Halloumi_Boxes.cpp

--- Halloumi_Boxes.cpp
+++ Halloumi_Boxes.cpp
@@ -34,3 +34,38 @@ int main()
         cout<<ans<<endl;
     }
 }
+
+
+
+
+//Without sorting 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    int a[n];
+	    for(int i = 0; i < n;i++){
+	        cin >> a[i];
+	    }
+	    bool canSort = true;
+        for (int i = 0; i < n - 1; i++) {
+            if (k == 1 && a[i] >= a[i + 1] && a[i] != a[i + 1] ) {
+                canSort = false;
+                break;
+            }
+        }
+        if (canSort) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+	   
+	}
+    return 0;
+}
